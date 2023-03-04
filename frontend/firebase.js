e86fea65354ab70afaa2ce696eef2909ed3dabd1
puepi <pmandel0001@yahoo.fr>
2023-03-04 14:35:30 +0100

    added firebase config file

---
+++ frontend/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCSYS3NCt-127Yz8e8Pvt6UeCaHJkpmLY",
+    authDomain: "clever-clone-95554.firebaseapp.com",
+    projectId: "clever-clone-95554",
+    storageBucket: "clever-clone-95554.appspot.com",
+    messagingSenderId: "615377607812",
+    appId: "1:615377607812:web:3a75fac82fb4bdb57f76d1"
+  };
